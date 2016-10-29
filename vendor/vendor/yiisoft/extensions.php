62560bdff5a461993f02461bd19e9ca7533c97f8
Zhetchko Vitchev <notargetbg@gmail.com>
2016-10-29 16:14:30 +0300

    vendor

---
+++ vendor/vendor/yiisoft/extensions.php
@@ -0,0 +1,15 @@
+<?php
+
+$vendorDir = dirname(__DIR__);
+
+return array (
+  'linslin/yii2-curl' => 
+  array (
+    'name' => 'linslin/yii2-curl',
+    'version' => '1.0.8.0',
+    'alias' => 
+    array (
+      '@linslin/yii2/curl' => $vendorDir . '/linslin/yii2-curl',
+    ),
+  ),
+);
