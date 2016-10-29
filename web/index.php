60de3cd3d528674ddce0910066c0483aa5d029e2
Zhetchko Vitchev <notargetbg@gmail.com>
2016-10-29 16:15:39 +0300

    debug & env

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_DEBUG') or define('YII_DEBUG', fasle);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
 // register Composer autoloader
