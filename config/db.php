834ea5b74f805f3a8f9cd5d58321f1f26f2a3ea0
lifewave <lifewave@pandora.superhosting.bg>
2016-10-29 16:27:52 +0300

    change-db-ignore

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=phrases-dota',
-    'username' => 'root',
-    'password' => 'root',
+    'dsn' => 'mysql:host=localhost;dbname=lifewave_phrases_dota',
+    'username' => 'lifewave_dota',
+    'password' => 'drift200sx',
     'charset' => 'utf8',
 ];
